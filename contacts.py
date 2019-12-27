1c5828fc05ce5b6ecf6d9cdc86d3c4ca6d21a777
Rohit <rohitpratti@gmail.com>
2019-12-27 00:20:34 -0800

    two different types of trie's

--- contacts.py
+++ contacts.py
@@ -7,7 +7,7 @@ class Node(object):
         self.num_usage = 0
 
 
-class Trie:
+class Trie2:
     def __init__(self):
         self.root = Node()
 
@@ -26,12 +26,63 @@ class Trie:
         return node.num_usage
 
 
-contacts = Trie()
+class Trie:
+    def __init__(self):
+        self.root = {}
+        self.total = []
 
+    def add(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node:
+                node[char] = {}
+            node = node[char]
+        node['*'] = True
+
+    def find(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node:
+                return False
+            node = node[char]
+        if '*' in node:
+            return True
+        else:
+            return False
+
+    def starts_with(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node:
+                return False
+            # self.total.append(len(node))
+            node = node[char]
+
+        return True
+
+    # def total_stars(self, word):
+    #     self.starts_with(word)
+    #     print(self.total)
+    #     return max(self.total)
+
+
+contacts = Trie()
+contacts2 = Trie2()
+contacts2.add("Rohit")
+contacts2.add("Ronald")
+contacts2.add("Robert")
+print(contacts2.find("Ro"))  # this finds total occurrences of substring
+
+
+contacts.add("Hack")
+contacts.add("Hackerrank")
+contacts.add("Hacking")
 contacts.add("Rohit")
-contacts.add("Ronald")
-contacts.add("Robert")
-contacts.add("Marth")
-print(contacts.find("Ro"))
-print(contacts.find("J"))
+contacts.add("Rohald")
+contacts.add("Ropon")
+print(contacts.find("Rohit"))  # finds if contact exists
+print(contacts.starts_with("Roh"))  # this finds if there is a contact starts with substring
+# print(contacts.total_stars("Hac"))
+# print(contacts.total_stars("Hak"))
+# print(contacts.root)
 
