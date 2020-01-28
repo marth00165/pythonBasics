62aa1772e89f81044688f78806798086e04c9e40
Rohit <rohitpratti@gmail.com>
2020-01-28 00:48:09 -0800

    working LRUcache not time efficient

--- lraCache.py
+++ lraCache.py
@@ -11,47 +11,48 @@ class LRUCache(object):
             value = self.cacheValue[key]
         else:
             return -1
-
-        print("here")
         for i in range(len(self.cacheObjects)):
             item = self.cacheObjects[i]
             for item_key, item_value in item.items():
                 if item_value == value and item_key == key:
                     self.cacheObjects.remove(item)
                     self.cacheObjects.append(item)
-            return [value, self.cacheObjects]
+        return [value, self.cacheObjects]
 
     def put(self, key: int, value: int) -> None:
         self.cacheValue[key] = value
         obj = {}
         obj[key] = value
 
-        for i in range(len(self.cacheObjects)):
-            item = self.cacheObjects[i]
-
-            if key in item.keys():
-                self.cacheObjects.remove(self.cacheObjects[i])
+        for item in self.cacheObjects:
+            for item_key, item_value in item.items():
+                if item_key == key:
+                    self.cacheObjects.remove(item)
 
         if len(self.cacheObjects) == self.capacity:
             item = self.cacheObjects[0]
-            for key in item:
-                self.cacheValue[key] = -1
+            for item_key in item:
+                self.cacheValue[item_key] = -1
             self.cacheObjects.remove(self.cacheObjects[0])
             self.cacheObjects.append(obj)
+            print(self.cacheObjects)
         else:
             self.cacheObjects.append(obj)
+            print(self.cacheObjects)
 
         self.cacheValue[key] = value
 
 
 cache = LRUCache(2)
-
-print(cache.get(2))
-cache.put(2, 6)
-print(cache.get(1))
-cache.put(1, 5)
-cache.put(1, 2)
+cache.put(2, 1)
+cache.put(1, 1)
+cache.put(2, 3)
+cache.put(4, 1)
 print(cache.get(1))
 print(cache.get(2))
+# cache.put(1, 2)
+# print(cache.get(1))
+# print(cache.get(3))
+# print(cache.get(4))
 
 
