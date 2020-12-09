8bbe5bc9488e327fbb46e94332ff813b2c3b953f
Rohit <rohitpratti@gmail.com>
2020-12-08 20:26:39 -0800

    01 matrix

---
+++ 01Matrix.py
@@ -0,0 +1,24 @@
+matrix = [[0, 0, 0], [0, 1, 0], [1, 2, 1]]
+
+# [
+# [0,0,0]
+# [0,1,0]
+# [1,1,1]
+# ]
+
+m = len(matrix)
+n = len(matrix[0])
+for i, row in enumerate(matrix):
+    for j, cell in enumerate(row):
+        if cell:
+            top = matrix[i-1][j] if i else float('inf')
+            left = matrix[i][j-1] if j else float('inf')
+            matrix[i][j] = min(top, left) + 1
+
+for i in reversed(range(m)):
+    for j in reversed(range(n)):
+        if cell := matrix[i][j]:
+            bottom = matrix[i+1][j] if i < m - 1 else float('inf')
+            right = matrix[i][j+1] if j < n - 1 else float('inf')
+            matrix[i][j] = min(cell, bottom + 1, right + 1)
+print(matrix)
